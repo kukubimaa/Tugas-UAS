e872904aeafb90981b071fa5efbc87712ac2592b
kukubimaa <38447802+kukubimaa@users.noreply.github.com>
2018-04-20 09:56:32 +0700

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,18 @@
+import kukubima.ifelse
+import kukubima.random
+import kukubima.append
+import kukubima.tupple
+import kukubima.dict
+import kukubima.opp
+
+print (kukubima.ifelse.fa())
+
+print (kukubima.random.fb())
+
+print (kukubima.append.fc())
+
+print (kukubima.tupple.fd())
+
+print (kukubima.dict.fe())
+
+print (kukubima.opp.ff())
